66514404f81ebc2fb5928a2af3eecf8b5103b8d8
colpaerte <eli.colpaert@delaware.pro>
2019-10-18 10:47:36 -0700

    lesson 1

--- lessons/core-concepts/1-hello-world/functions/hello-world.js
+++ lessons/core-concepts/1-hello-world/functions/hello-world.js
@@ -1,4 +1,3 @@
-
 exports.handler = (event, context, callback) => {
   /* Step 3. In this_file, Create a `200` response code and return the `event` data in the response body.
 
@@ -6,6 +5,12 @@ exports.handler = (event, context, callback) => {
 
     For more details, see the [docs](https://bit.ly/2uEFyKU)
   */
-  const response = {}
-  return callback(null, response)
-}
+  const response = {
+    statusCode: 200,
+    headers: {
+      "x-custom-header": "My Header Value"
+    },
+    body: JSON.stringify({ message: "Hello Eli!" })
+  };
+  return callback(null, response);
+};
